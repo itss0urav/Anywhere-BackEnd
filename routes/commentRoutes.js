715647610799574894d4b2911069f28b72dc5cf6
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-04-07 19:38:31 +0530

    Get comment controller added

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
+const secure = require("../middlewares/authorization")
+const {createComment, getComments} = require("../controllers/commentController")
 
-// router.route("/").post()
+
+router.route("/").post(secure, createComment).get(getComments)
+
+
+module.exports = router
