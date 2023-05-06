e52e917f73ae1c61e34bde8f202d37c5c3b32996
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-05-06 11:03:27 +0530

    Verificatioj completed

---
+++ controllers/verificationController.js
@@ -0,0 +1,33 @@
+const asyncHandler = require("express-async-handler")
+const Verification = require("../models/verification")
+
+const getVerificationRequests = asyncHandler(async (req, res) => {
+
+const requests = await Verification.find({}).populate("userId")
+
+return res.status(200).send(requests)
+})
+
+//controller for creating new verification requests
+
+const createVerificationRequests = asyncHandler(async (req, res) => {
+
+const result = await Verification.create(req.body)
+
+return res.status(200).send(result)
+})
+
+//controller for updating the approval state
+
+const updateApprovalState = asyncHandler(async (req, res) => {
+
+    const {_id} = req.body
+
+    delete req.body["_id"]
+
+    const result = await Verification.findByIdAndUpdate(_id, req.body)
+
+    return res.status(200).send(result)
+})
+
+module.exports = {getVerificationRequests, createVerificationRequests, updateApprovalState}
